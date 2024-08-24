354c635f8a0224befe53ce1d6713089d35d9c20f
Yongjun <wangyj641@gmail.com>
2024-08-24 16:32:20 +0800

    Add NavBar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TabBar } from 'antd-mobile';
-import { BsHouseDoorFill, BsHouseDoor, BsBagFill, BsBag, BsPersonFill, BsPerson } from 'react-icons/bs';
+import { BsHouseDoor, BsBag, BsPerson } from 'react-icons/bs';
 import { history, useLocation } from 'umi';
 
 export interface ItemProps {
@@ -21,19 +21,16 @@ const MenuBar = (props: ItemProps) => {
   const items = [
     {
       title: 'Home',
-      selectedIcon: <BsHouseDoorFill style={{ fontSize: '1.5rem' }} />,
       icon: <BsHouseDoor style={{ fontSize: '1.5rem' }} />,
       link: '/'
     },
     {
       title: 'Order',
-      selectedIcon: <BsBagFill style={{ fontSize: '1.5rem' }} />,
       icon: <BsBag style={{ fontSize: '1.5rem' }} />,
       link: '/order'
     },
     {
       title: 'User',
-      selectedIcon: <BsPersonFill style={{ fontSize: '1.5rem' }} />,
       icon: <BsPerson style={{ fontSize: '1.5rem' }} />,
       link: '/user'
     }
@@ -52,7 +49,6 @@ const MenuBar = (props: ItemProps) => {
               <TabBar.Item
                 key={item.link}
                 icon={item.icon}
-                selectedIcon={item.selectedIcon}
                 title={item.title}
               />
             )
